e5ea1cf6e761e1c04300315ef499a2b8d351f7c7
Сергей Климович <sergei.klimovich@gmail.com>
2021-06-05 09:26:24 +0300

    Add Sign UP View

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var email = ""
+    @State var password = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+           Image("logo")
+            .resizable()
+            .frame(width: 184, height: 184, alignment: .center)
+            .padding(.bottom, 26)
+            Text("EMAIL")
+                .frame(width: UIScreen.main
+                        .bounds.width-50, height: nil, alignment: .leading)
+            HStack {
+                Image(systemName: "envelope")
+                    .font(.title2)
+                    .padding(.leading)
+                TextField("name@email.com", text: $email)
+            }
+            .frame(width: UIScreen.main
+                    .bounds.width-50, height: 48, alignment: .center)
+            .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
+            Text("PASSWORD")
+                .frame(width: UIScreen.main
+                        .bounds.width-50, height: nil, alignment: .leading)
+                .padding(.top, 15)
+            HStack {
+                Image(systemName: "lock")
+                    .font(.title2)
+                    .padding(.leading)
+                TextField("***********", text: $password)
+            }
+            .frame(width: UIScreen.main
+                    .bounds.width-50, height: 48, alignment: .center)
+            .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 1))
+            
+            
+            Button(action: {
+                
+            }, label: {
+                Text("SIGN UP")
+                    .foregroundColor(.white)
+                    .frame(width: 335, height: 52, alignment: .center)
+                    .background(Color("AccentColor"))
+                    .cornerRadius(50)
+            }).padding(.top)
+            
+            HStack {
+                Text("Already have an account?")
+                Text("Login")
+                    .foregroundColor(.blue)
+            }.padding(.top)
+        }
     }
 }
 
