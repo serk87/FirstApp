c1ad1082b691546963d71ef6f663957b86583a3a
Сергей Климович <sergei.klimovich@gmail.com>
2021-06-05 16:01:46 +0300

    10

--- FirstApp/HomeView.swift
+++ FirstApp/HomeView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Image(systemName: "bookmark")
+                Spacer()
+                Text("Social").font(.title)
+                Spacer()
+                Image(systemName: "bell")
+            }.padding()
+        }
     }
 }
 
